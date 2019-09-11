49ea398f0bc3edac0e59628a6e44efd2728a997f
cryz97 <paladincryz@gmail.com>
2019-09-11 16:30:29 -0500

    Se añadieron parametros iniciales y 3 clases de imagenes para probar el tensorflow

---
+++ main.py
@@ -0,0 +1,27 @@
+import sys
+import os
+from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.python.keras import optimizers
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dropout, Flatten, Dense, Activation
+from tensorflow.python.keras.layers import  Convolution2D, MaxPooling2D
+from tensorflow.python.keras import backend as K
+
+K.clear_session()
+
+data_entrenamiento = '/assets/training'
+data_validacion = '/assets/validation'
+
+epocas=20
+longitud, altura = 150, 150
+batch_size = 32
+pasos = 1000
+validation_steps = 300
+filtrosConv1 = 32
+filtrosConv2 = 64
+tamano_filtro1 = (3, 3)
+tamano_filtro2 = (2, 2)
+tamano_pool = (2, 2)
+clases = 3
+lr = 0.0004
+
